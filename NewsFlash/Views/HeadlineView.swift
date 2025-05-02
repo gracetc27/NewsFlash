5a1de1ef88c9548fea7d9f803ec997a0eb9d07f2
Grace Couch <gracetcouch@gmail.com>
2025-05-02 12:11:57 +0100

    initial file set up

---
+++ NewsFlash/Views/HeadlineView.swift
@@ -0,0 +1,18 @@
+//
+//  HeadlineView.swift
+//  NewsFlash
+//
+//  Created by Grace couch on 02/05/2025.
+//
+
+import SwiftUI
+
+struct HeadlineView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HeadlineView()
+}
