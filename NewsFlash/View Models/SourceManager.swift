27ad4a7b2a81315d66770d6c24b86f9f118dade2
Grace Couch <gracetcouch@gmail.com>
2025-05-08 16:58:14 +0100

    saving in source manager

--- NewsFlash/View Models/SourceManager.swift
+++ NewsFlash/View Models/SourceManager.swift
@@ -8,4 +8,37 @@ import Foundation
 
 class SourceManager {
     var userSelectedSources: Set<String> = []
+
+    private let savePath = URL.documentsDirectory.appendingPathComponent("sources")
+
+
+    func loadSavedSources() async {
+        do {
+            let data = try Data(contentsOf: savePath)
+            userSelectedSources = try JSONDecoder().decode(Set<String>.self, from: data)
+        } catch {
+            print(error.localizedDescription)
+            userSelectedSources = []
+        }
+    }
+
+    func saveSource(_ source: Source) {
+        do {
+            userSelectedSources.insert(source.id)
+            let data = try JSONEncoder().encode(userSelectedSources)
+            try data.write(to: savePath, options: [.atomic, .completeFileProtection])
+        } catch {
+            print("Unable to save data.")
+        }
+    }
+
+    func removeSavedSource(_ source: Source) {
+        do {
+            userSelectedSources.remove(source.id)
+            let data = try JSONEncoder().encode(userSelectedSources)
+            try data.write(to: savePath, options: [.atomic, .completeFileProtection])
+        } catch {
+            print(error)
+        }
+    }
 }
